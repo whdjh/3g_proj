3032db46a8e560d971003921ae127fb46fbd4bdb
kimhotac <114737603+kimhotac@users.noreply.github.com>
2023-10-11 13:07:58 +0900

    start (#9)

---
+++ raptor/editor/small.js
@@ -0,0 +1,28 @@
+var myCanvas = document.getElementById('myCanvas');
+resize();
+var graphics = myCanvas.getContext('2d');
+var mouse = {};
+window.addEventListener("mousemove", function(event){
+    mouse.x = event.clientX+c.width;
+    mouse.y = event.clientY;
+});
+window.addEventListener("mousedown", function(){
+    mouse.down = true;
+    mouse.up = false;
+});
+window.addEventListener("mouseup", function(){
+    mouse.down = false;
+    mouse.up = true;
+});
+
+graphics.clear = function(){
+    this.clearRect(0,0,myCanvas.width,myCanvas.height);
+}
+
+function GET(m){
+    return prompt(m);
+}
+//2023.10.11 (김호탁) PUT -> print
+function print(m){
+    console.log(m);
+}
